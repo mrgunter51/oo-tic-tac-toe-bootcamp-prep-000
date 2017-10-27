9c824d18f29e1a005238e673474eac9c919a8744
mrgunter51 <mrgunter@crimson.ua.edu>
2017-10-27 04:06:36 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,8 +54,8 @@ class TicTacToe
     @board.count("X") + @board.count("O")
   end
 
-  def current_player(board)
-    if turn_count(board) % 2 == 0
+  def current_player
+    if turn_count % 2 == 0
       "X"
     else
       "O"
